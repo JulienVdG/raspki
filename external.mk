238e153413514a626201298ceb624ca6e89ecfed
Julien Viard de Galbert <julien@vdg.name>
2015-05-03 14:33:29 +0200

    Add specific deploy rules (sd and ssh)

--- external.mk
+++ external.mk
@@ -3,3 +3,14 @@
 BR2_ROOTFS_POST_BUILD_SCRIPT += $(BR2_EXTERNAL)/ssh_devel.sh
 
 BR2_ROOTFS_POST_IMAGE_SCRIPT += $(BR2_EXTERNAL)/update_rpi_config.sh
+
+DEPLOY_FILES = $(BINARIES_DIR)/rpi-firmware/* $(BINARIES_DIR)/zImage $(BINARIES_DIR)/rootfs.cpio.gz
+DEPLOY_SSH_OPTS ?= -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"
+DEPLOY_SSH_USERHOST ?= root@$(BR2_TARGET_GENERIC_HOSTNAME)
+
+deploy-sd:
+	if [ -d $(DEPLOY_DIR) ]; then cp -R $(DEPLOY_FILES) $(DEPLOY_DIR); fi
+
+deploy-ssh:
+	scp $(DEPLOY_SSH_OPTS) -r $(DEPLOY_FILES) $(DEPLOY_SSH_USERHOST):/boot
+
