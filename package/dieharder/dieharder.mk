b4b1b6bcc9d86f5bfbbaaaef3b89caedc49e0adc
Julien Viard de Galbert <julien@vdg.name>
2016-09-25 23:03:20 +0200

    fix for dieharder package

--- package/dieharder/dieharder.mk
+++ package/dieharder/dieharder.mk
@@ -7,21 +7,20 @@
 DIEHARDER_VERSION = 3.31.1
 DIEHARDER_SITE = http://www.phy.duke.edu/~rgb/General/dieharder
 DIEHARDER_SOURCE = dieharder-$(DIEHARDER_VERSION).tgz
-DIEHARDER_LICENSE = GPLv2
-DIEHARDER_LICENSE_FILES = GPL
-DIEHARDER_DEPENDENCIES = gsl
 DIEHARDER_SUBDIR = dieharder-$(DIEHARDER_VERSION)
-DIEHARDER_AUTORECONF = YES
+DIEHARDER_LICENSE = GPLv2 with beverage clause
+DIEHARDER_LICENSE_FILES = $(DIEHARDER_SUBDIR)/COPYING
+DIEHARDER_DEPENDENCIES = gsl
 
+#DIEHARDER_CONF_OPTS = --includedir=$(STAGING_DIR)/usr/include
 # fix endiannes detection
 ifeq ($(BR2_ENDIAN),"BIG")
-BR2_AC_CV_C_ENDIAN = ac_cv_c_endian=bin
+DIEHARDER_CONF_ENV = ac_cv_c_endian=big
 else
-BR2_AC_CV_C_ENDIAN = ac_cv_c_endian=little
+DIEHARDER_CONF_ENV = ac_cv_c_endian=little
 endif
-DIEHARDER_CONF_OPTS = $(BR2_AC_CV_C_ENDIAN)
 
 # parallel build fail, disable it
-DIEHARDER_MAKE=$(MAKE1)
+DIEHARDER_MAKE = $(MAKE1)
 
 $(eval $(autotools-package))
