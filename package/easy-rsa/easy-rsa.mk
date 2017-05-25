8a4f89e5a7c7e0af494556d954e0ae6450decbed
Julien Viard de Galbert <julien@vdg.blogsite.org>
2017-05-25 09:05:46 +0200

    easy-rsa: new package

---
+++ package/easy-rsa/easy-rsa.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# easy-rsa
+#
+################################################################################
+
+EASY_RSA_VERSION = 3.0.1
+EASY_RSA_SOURCE = EasyRSA-$(EASY_RSA_VERSION).tgz
+EASY_RSA_SITE = https://github.com/OpenVPN/easy-rsa/releases/download/$(EASY_RSA_VERSION)
+
+define EASY_RSA_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/easyrsa $(TARGET_DIR)/usr/bin
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/easy-rsa/x509-types
+    $(INSTALL) -D -m 0644 $(@D)/x509-types/* $(TARGET_DIR)/etc/easy-rsa/x509-types
+    $(INSTALL) -D -m 0644 $(@D)/openssl-1.0.cnf $(TARGET_DIR)/etc/easy-rsa
+    $(INSTALL) -T -m 0644 $(@D)/vars.example $(TARGET_DIR)/etc/easy-rsa/vars
+endef
+
+$(eval $(generic-package))
